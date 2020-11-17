c4881e91b8bcb74e6549d4684bf3197ea1cb35c3
白云辉 <kooksee@163.com>
2020-11-17 23:13:51 +0800

    第一版本构建

--- version/version.go
+++ version/version.go
@@ -1,35 +1,4 @@
 package version
 
-import (
-	"runtime"
-
-	"github.com/pubgo/xerror"
-
-	ver "github.com/hashicorp/go-version"
-	"github.com/pubgo/catdog/catdog_version"
-)
-
-var BuildTime = ""
-var Version = ""
-var GoVersion = runtime.Version()
-var GoPath = ""
-var GoROOT = ""
-var CommitID = ""
-var Project = ""
-
-func init() {
-	if Version == "" {
-		Version = "v0.0.1"
-	}
-
-	xerror.ExitErr(ver.NewVersion(Version))
-	xerror.Exit(catdog_version.Register("catdog_version", catdog_version.M{
-		"build_time": BuildTime,
-		"version":    Version,
-		"go_version": GoVersion,
-		"go_path":    GoPath,
-		"go_root":    GoROOT,
-		"commit_id":  CommitID,
-		"project":    Project,
-	}))
-}
+const BuildTime = "2020-11-17 23:13:06"
+const Version = "v0.0.2"
